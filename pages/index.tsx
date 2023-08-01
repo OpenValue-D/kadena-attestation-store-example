8743fdfa59ce6656a98062ef733c7ce64b507640
Konstantin Gubanov <konstantin@openvalue.de>
2023-08-01 17:32:52 +0200

    Add footer with kadena and greps logos

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,10 @@ import AppNavbar from "../app/components/AppNavbar";
 import "bootstrap/dist/css/bootstrap.css"
 import "./globals.css"
 import CertSearchAndShow from "@/app/components/CertSearchAndShow";
+import Footer from "@/app/components/Footer";
 
 export default function Home() {
   return (
-    <><AppNavbar/><CertSearchAndShow/></>
+    <><AppNavbar/><CertSearchAndShow/><Footer/></>
   )
 }
